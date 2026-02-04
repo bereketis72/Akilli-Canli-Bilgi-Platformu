842e601445068c721de47da14f9f5161f12764bc
Bereket İş <bereketis54@gmail.com>
2026-02-04 10:37:43 +0300

    Veritabanı tablosu ve SQL şeması eklendi

--- bilgi 2.sql
+++ bilgi 2.sql
@@ -1,3 +1,4 @@
+create database bilgi_platform
 USE bilgi_platform;
 
 CREATE TABLE canlinin_bilgileri(
@@ -10,21 +11,3 @@ CREATE TABLE canlinin_bilgileri(
     kategori NVARCHAR(50) DEFAULT 'Diger',
     kayit_tarihi DATETIME DEFAULT GETDATE()
 );
-
---create database bilgi_platform
-/*
-use bilgi_platform
-CREATE TABLE canli_bilgileri (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    baslik NVARCHAR(255) UNIQUE,   
-    giris_ozeti NVARCHAR(MAX),       
-    genel_ozet NVARCHAR(MAX),        
-    tam_metin NVARCHAR(MAX),         
-    url NVARCHAR(500),              
-    kayit_tarihi DATETIME DEFAULT GETDATE()
-);
-
-
-ALTER TABLE canli_bilgileri
-ADD kategori NVARCHAR(50) DEFAULT 'Diger';
-*/
